06d6687b46bb460fb542d35d663af7b4345f68c5
wangpeng <383557980@qq.com>
2019-02-21 00:06:09 +0800

    '添加地图模式为卫星图'

--- js/index.js
+++ js/index.js
@@ -7,22 +7,23 @@ var deg_img = document.querySelector('.deg-img')
 var pan = document.querySelector('.main-pan')
 
 if (/android/.test(ua)) {
-	window.addEventListener('deviceorientationabsolute',DeviceOrientationHandlerCompass, false);
-	window.addEventListener('deviceorientationabsolute',DeviceOrientationHandlerDeg_img, false);
+	window.addEventListener('deviceorientationabsolute', DeviceOrientationHandlerCompass, false);
+	window.addEventListener('deviceorientationabsolute', DeviceOrientationHandlerDeg_img, false);
 
 	function DeviceOrientationHandlerCompass(event) {
 		console.log(event);
 		// document.getElementById("alpha").innerHTML = Math.round(360 - event.alpha);
 		compass.style.transform = 'rotate(-' + Math.round(360 - event.alpha) + 'deg)'
 	}
+
 	function DeviceOrientationHandlerDeg_img(event) {
 		// document.getElementById("alpha").innerHTML = Math.round(360 - event.alpha);
 		deg_img.style.transform = 'rotate(-' + Math.round(360 - event.alpha) + 'deg)'
-	
+
 	}
-	
+
 } else {
-	window.addEventListener('deviceorientation', function(){
+	window.addEventListener('deviceorientation', function() {
 		DeviceOrientationHandlerCompass(event)
 		DeviceOrientationHandlerDeg_img(event)
 	}, false);
@@ -31,26 +32,28 @@ if (/android/.test(ua)) {
 		// document.getElementById("alpha").innerHTML = Math.round(360 - event.alpha);
 		compass.style.transform = 'rotate(-' + event.webkitCompassHeading + 'deg)'
 	}
-	
+
 	function DeviceOrientationHandlerDeg_img(event) {
 		// document.getElementById("alpha").innerHTML = Math.round(360 - event.alpha);
 		deg_img.style.transform = 'rotate(-' + event.webkitCompassHeading + 'deg)'
-	
+
 	}
 }
 
 // 手动
 var manual = document.getElementById("manual")
 var isManual = false
-manual.addEventListener('touchend',()=>{
+manual.addEventListener('touchstart', () => {
 	// 取消罗盘监听事件
-	if(isManual){
-		window.addEventListener('deviceorientationabsolute',DeviceOrientationHandlerCompass, false);
+	if (isManual) {
+		window.addEventListener('deviceorientationabsolute', DeviceOrientationHandlerCompass, false);
 		console.log('add');
-	}else{
-		window.removeEventListener('deviceorientationabsolute',DeviceOrientationHandlerCompass)
+	} else {
+		window.removeEventListener('deviceorientationabsolute', DeviceOrientationHandlerCompass, false)
 	}
 	isManual = !isManual
+
+	console.log('isManual', isManual)
 })
 
 // 监听手动转盘数据
@@ -59,29 +62,36 @@ var panOffsetTop = pan.getBoundingClientRect().top
 var panOffsetLeft = pan.getBoundingClientRect().left
 var panOffsetRight = pan.getBoundingClientRect().right
 var panWidth = pan.offsetWidth / 2 //罗盘一半的宽度
-var touchX,touchY; //用于记录手指按下位置，判断手指在罗盘哪个位置
-pan.addEventListener('touchstart',function(e){
+var touchX, touchY; //用于记录手指按下位置，判断手指在罗盘哪个位置
+pan.addEventListener('touchstart', function(e) {
 	var clientY = e.changedTouches[0].pageY
 	var clientX = e.changedTouches[0].pageX
-	var currentClentY = clientY-panOffsetTop //手指在当前盘上竖向的位置
-	var currentClentX = clientX-panOffsetLeft //手指在当前盘上横向位置
-	console.log(clientX,panWidth);
-	
+	var currentClentY = clientY - panOffsetTop //手指在当前盘上竖向的位置
+	var currentClentX = clientX - panOffsetLeft //手指在当前盘上横向位置
+	console.log(clientX, panWidth);
+
 })
-pan.addEventListener('touchmove',function(e){
-	if(!isManual){
+pan.addEventListener('touchmove', function(e) {
+	console.log('isManual', isManual)
+	if (!isManual) {
 		return
 	}
+
+	console.log('touchmove')
 	var clientY = e.changedTouches[0].pageY
 	var clientX = e.changedTouches[0].pageX
-	var currentClentY = clientY-panOffsetTop //手指在当前盘上竖向的位置
-	var currentClentX = clientX-panOffsetLeft //手指在当前盘上横向位置
-	if(currentClentX > panWidth){
+	var currentClentY = clientY - panOffsetTop //手指在当前盘上竖向的位置
+	var currentClentX = clientX - panOffsetLeft //手指在当前盘上横向位置
+	if (currentClentX > panWidth) {
 		//在右侧拖动
 		compass.style.transform = 'rotate(' + Math.round(currentClentY) + 'deg)'
-	}else{
+	} else {
 		compass.style.transform = 'rotate(-' + Math.round(currentClentY) + 'deg)'
 	}
-	
-	console.log('currentClentX',currentClentX);
+
+	console.log('currentClentX', currentClentX);
 })
+
+document.body.addEventListener("touchmove", function(event) {
+	event.preventDefault();
+});
