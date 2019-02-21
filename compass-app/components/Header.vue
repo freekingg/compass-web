7d5ad4c6aa6768c44ab3fafe60694398e59fc5ed
王鹏 <383557980@qq.com>
2019-02-21 17:57:13 +0800

    'app'

---
+++ compass-app/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<image class="header" src="/static/images/logo.png"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:'Header',
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+	.header{
+		width: 100%;
+		height: 100upx;
+	}
+</style>
---
+++ compass-app/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="content">
+		<!-- <wHeader/> -->
+		<web-view src="https://ii.sinelinked.com/compass-web/"></web-view>
+	</view>
+</template>
+
+<script>
+	import wHeader from "../../components/Header.vue"
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		components:{
+			wHeader
+		}
+	}
+</script>
+
+<style>
+</style>
